a1e2ace7e86e5f0d7f145d6e02723dd9e0a51daf
melisaozgur@posta.mu.edu.tr <melisaozgur@posta.mu.edu.tr>
2024-12-02 01:38:23 +0300

    Initial Commit

---
+++ SkinCheck/SkinCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCheckApp.swift
+//  SkinCheck
+//
+//  Created by Melisa Özgür on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SkinCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkinCheckTests/SkinCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCheckTests.swift
+//  SkinCheckTests
+//
+//  Created by Melisa Özgür on 2.12.2024.
+//
+
+import Testing
+@testable import SkinCheck
+
+struct SkinCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
